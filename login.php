4fca0a99058ffb62f77921489b0907aceab72c97
IlTeLp <146825103+IlTeLp@users.noreply.github.com>
2023-10-03 19:56:20 +0300

    registration
    registration

--- login.php
+++ login.php
@@ -5,6 +5,7 @@ if(!$baza){
 }
 $name=$_POST['user'];
 $password=md5($_POST['pass']);
+$chek=mysqli_query($baza,"SELECT * FROM `users` WHERE `login`='$name' AND `password_hash`='$password'");
 if(mysqli_num_rows($chek)>0){
     echo "Вы успешно вошли!";
 }
